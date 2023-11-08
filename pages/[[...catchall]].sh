aac06c9b11a006973bbb8c7d26e82841e17f19c1
Sarah Schulte <git@sarah.engineer>
2023-11-08 09:52:44 -0800

    its fine

--- pages/[[...catchall]].sh
+++ pages/[[...catchall]].sh
@@ -6,6 +6,8 @@ REPO_NAME="${BASH_REMATCH[2]}"
 REPO_OWNER="${BASH_REMATCH[1]}"
 
 header Location "/$REPO_OWNER/$REPO_NAME"
+
+end_headers
 end_headers
 
 return $(status_code 301)
--- pages/oauth.sh
+++ pages/oauth.sh
@@ -1,7 +1,6 @@
+# headers
 
 source config.sh
-
-debug "CLIENT ID: $CLIENT_ID"
 
 CURL_RESPONSE="$(curl -Ss -X POST "https://github.com/login/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${QUERY_PARAMS[code]}&redirect_uri=https://github2.com/oauth")"
 
@@ -14,3 +13,7 @@ ACCESS_TOKEN="${FORM_DATA[access_token]}"
 SESSION[access_token]="$ACCESS_TOKEN"
 
 save_session
+
+header Location '/'
+end_headers
+end_headers
